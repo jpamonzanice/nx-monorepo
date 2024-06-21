194a84f4b829ad8751621af5a3545d642df78654
Juan Pablo Amonzabel <Juan.Amonzabel@niceincontact.com>
2024-06-20 22:59:16 -0400

    fixed nx graph for angular remotes

--- apps/store/src/app/app.routes.ts
+++ apps/store/src/app/app.routes.ts
@@ -1,18 +1,15 @@
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
-import { loadRemoteModule } from '@nx/angular/mf';
 import { WebComponentWrapper, WebComponentWrapperOptions } from '@angular-architects/module-federation-tools';
 
 export const appRoutes: Route[] = [
   {
     path: 'cart',
-    loadChildren: () =>
-      loadRemoteModule('cart', './Routes').then((m) => m.remoteRoutes),
+    loadChildren: () => import('cart/Routes').then((m) => m.remoteRoutes),
   },
   {
     path: 'about',
-    loadChildren: () =>
-      loadRemoteModule('about', './Routes').then((m) => m.remoteRoutes),
+    loadChildren: () => import('about/Routes').then((m) => m.remoteRoutes),
   },
   {
     path: 'product',
